d5d019ce955d336ae6c85e491a68ffa3b6d861c2
Glyde <glydeb@yahoo.com>
2016-06-29 16:39:33 -0500

    end of day save

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ var user = require('./routes/user');
 var register = require('./routes/register');
 var download = require('./routes/download');
 var verification = require('./routes/verification');
+var sms = require('./routes/sms');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -38,6 +39,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
+app.use('/sms', sms);
 app.use('/verification', verification);
 app.use('/register', register);
 app.use('/user', user);
