a7d6e6bd1e4ebda4c10718a1c235ef3c0fb544b0
Glyde <glydeb@yahoo.com>
2016-07-01 16:06:04 -0500

    changes to Factory/login

--- server/public/assets/scripts/controllers/landingpageController.js
+++ server/public/assets/scripts/controllers/landingpageController.js
@@ -3,29 +3,29 @@ myApp.controller('landingpageController', ['$scope', '$http', '$window',
     $scope.downloadsArray = [];
     getDownloads();
 
-    function getDownloads () {
-    $http.get('/download').then(function (response) {
-      console.log('getting downloads');
-      console.log(response.data);
-      $scope.downloadsArray = response.data;
-    });
-  };
-
-  // This happens after view/controller loads -- not ideal
-  console.log('checking user');
-  $http.get('/user').then(function (response) {
-    if (response.data.username) {
-      $scope.userName = response.data.username;
-      console.log('User Data: ', $scope.userName);
-    } else {
-      $location.path('/home');
+    function getDownloads() {
+      $http.get('/download').then(function (response) {
+        console.log('getting downloads');
+        console.log(response.data);
+        $scope.downloadsArray = response.data;
+      });
     }
-  });
 
-  $scope.logout = function () {
-    $http.get('/user/logout').then(function (response) {
-      console.log('logged out');
-      $location.path('/home');
+    // This happens after view/controller loads -- not ideal
+    console.log('checking user');
+    $http.get('/user').then(function (response) {
+      if (response.data.username) {
+        $scope.userName = response.data.username;
+        console.log('User Data: ', $scope.userName);
+      } else {
+        $location.path('/home');
+      }
     });
-  };
+
+    $scope.logout = function () {
+      $http.get('/user/logout').then(function (response) {
+        console.log('logged out');
+        $location.path('/home');
+      });
+    };
 }]);
--- server/public/assets/scripts/factories/doGoodFactory.js
+++ server/public/assets/scripts/factories/doGoodFactory.js
@@ -1,27 +1,27 @@
-myApp.factory('FantasyFactory', ['$http', function ($http) {
-  console.log('fantasyFactory running');
+myApp.factory('doGoodFactory', ['$http', function ($http) {
+  console.log('doGood Factory online!');
   var user = undefined;
 
-  function refreshUserData () {
-    var promise = $http.get('/user').then(function(response) {
+  function refreshUserData() {
+    var promise = $http.get('/user').then(function (response) {
 
       user = response.data;
-      
+
     });
 
     return promise;
 
   }
 
-
-
   var publicApi = {
     factoryRefreshUserData: function () {
       return refreshUserData();
     },
+
     factoryGetUserData: function () {
-        return user;
-      }
+      return user;
+    }
+
   };
   return publicApi;
 }]);
