e60eaa13cc6b5257dc7c4c3cc254603fa1c445c7
Quinn Nelson <quinn.nelson1@gmail.com>
2016-07-03 12:32:46 -0500

    Further changes to family info on our profile page

--- server/public/assets/scripts/controllers/profileController.js
+++ server/public/assets/scripts/controllers/profileController.js
@@ -5,15 +5,23 @@ myApp.controller('profileController', ['doGoodFactory', '$scope', '$http',
   console.log('profileController running');
 
     $scope.user = {};
-    $scope.user.family_members = ""
+    $scope.user.family_members = "Click edit to add members to your family"
     $scope.user.about_us = "Click edit to write a bio about your family"
-    $scope.user.our_projects = "Click edit to add projects as you complete them"
+    $scope.user.our_projects = "Click edit to showcase your recent projects"
     $scope.edit = false;
     $scope.visible = true;
     $scope.toggle = function() {
       $scope.visible = !$scope.visible
       $scope.edit = !$scope.edit
-    };
+      $http.put('/register', $scope.user).then(function (response) {
+        console.log("family info saved");
+    })};
+
+//     $scope.saveInfo = function() {
+//       console.log("saveinfo");
+//     $http.post('/', $scope.user).then(function (response) {
+//       console.log("family info saved");
+// })};
 
   $(document).ready(function () {
     $('.materialboxed').materialbox();
